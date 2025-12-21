ef607e9c24afbe6965c18bea4c9d15021e21c573
Rashad Mirza <rashad.mirza.404@gmail.com>
2025-12-21 00:27:34 -0600

    path fix

--- start-queue.sh
+++ start-queue.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pgrep -f "queue:work" > /dev/null || (cd /home/ugn/api.alert.az && /usr/local/bin/ea-php82 artisan queue:work --sleep=3 --tries=100 >> storage/logs/queue.log 2>&1 &)
+pgrep -f "queue:work" > /dev/null || (cd /home/ugn/api.alert.az && /usr/local/bin/ea-php82 artisan queue:work --sleep=3 --tries=100 >> /home/ugn/api.alert.az/storage/logs/queue.log 2>&1 &)
